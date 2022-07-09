63e58eb40b224bcc4d9ca2909753a80de5361b47
ananyaativ <84693150+ananyaativ@users.noreply.github.com>
2022-07-09 10:09:18 +0530

    Update App.js
    Add button to connect to different webpage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [vegetable, setVegetable] = useState([]);
-  const [fruit, setFruit] = useState([]);
-  const [checked, setChecked] = useState([]);
+    const [vegetable, setVegetable] = useState([]);
+    const [fruit, setFruit] = useState([]);
+    const [checked, setChecked] = useState([]);
 
     // Add/Remove checked item from list
     const handleCheck = (event) => {
@@ -19,51 +19,53 @@ const App = () => {
         setChecked(updatedList);
     };
 
-  const fetchData = () => {
-    const vegetableAPI = "http://localhost:8080/filter?type=vegetable";
-    const fruitAPI = "http://localhost:8080/filter?type=fruit";
+    const fetchData = () => {
+        const vegetableAPI = "http://localhost:8080/filter?type=vegetable";
+        const fruitAPI = "http://localhost:8080/filter?type=fruit";
 
-    const getVegetable = axios.get(vegetableAPI);
-    const getFruit = axios.get(fruitAPI);
-    axios.all([getVegetable, getFruit]).then(
-      axios.spread((...allData) => {
+        const getVegetable = axios.get(vegetableAPI);
+        const getFruit = axios.get(fruitAPI);
+        axios.all([getVegetable, getFruit]).then(
+            axios.spread((...allData) => {
 
-        const vegetableData = allData[0].data;
-        const fruitData = allData[1].data;
+                const vegetableData = allData[0].data;
+                const fruitData = allData[1].data;
 
-        setVegetable(vegetableData);
-        setFruit(fruitData);
+                setVegetable(vegetableData);
+                setFruit(fruitData);
 
-      })
-    )
+            })
+        )
 
-  }
-   //checked is the list of ingredients name that are checked 
+    }
+    //checked is the list of ingredients name that are checked
     console.log(checked);
 
     useEffect(() =>{
-    fetchData()
-  }, [])
+        fetchData()
+    }, [])
 
-  return (
-    <div className="App">
-        <h2>Vegetables</h2>
-        {vegetable.map((element) => <label>
-                <input value={element.name} type="checkbox" onChange={handleCheck}/>
-                {element.name} <br/>
-        </label>
-        )}
-        <h2>Fruits</h2>
-        {fruit.map((element) => <label>
-                <input value={element.name} type="checkbox" onChange={handleCheck}/>
-                {element.name} <br/>
-            </label>
+    return (
+        <div className="App">
+            <h2>Vegetables</h2>
+            {vegetable.map((element) => <label>
+                    <input value={element.name} type="checkbox" onChange={handleCheck}/>
+                    {element.name} <br/>
+                </label>
+            )}
+            <h2>Fruits</h2>
+            {fruit.map((element) => <label>
+                    <input value={element.name} type="checkbox" onChange={handleCheck}/>
+                    {element.name} <br/>
+                </label>
 
-        )}
-     <br/>
+            )}
+            <br/>
+            //To DO: Add the event to change to different page and pass the list of checked ingredients
+            <button>Search</button>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
